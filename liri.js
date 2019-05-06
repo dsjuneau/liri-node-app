168f4ae63c9fd5fd006d1a9e9206afa26f924f56
Scott Juneau <scott@junlaw.com>
2019-05-06 17:03:06 -0500

    Basic files set up

---
+++ liri.js
@@ -0,0 +1,48 @@
+var environment = require("dotenv").config();
+var fs = require("fs");
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+
+let inputSelector = [
+  "concert-this",
+  "spotify-this-song",
+  "movie-this",
+  "do-what-it-says"
+];
+
+let selector = process.argv[2].toLowerCase();
+let inputValue = process.argv[3].toLowerCase();
+
+//Take action based upon what is input.
+switch (inputSelector.indexOf(selector)) {
+  case 0:
+    concert(inputValue);
+    break;
+  case 1:
+    song(inputValue);
+    break;
+  case 2:
+    movie(inputValue);
+    break;
+  case 3:
+    whatever(inputValue);
+    break;
+  case -1:
+    break;
+}
+
+function concert(concert) {
+  console.log("looking for concert: " + concert);
+}
+
+function song(song) {
+  console.log("looking for song: " + song);
+}
+
+function movie(movie) {
+  console.log("looking for movie: " + movie);
+}
+
+function whatever(whatever) {
+  console.log("looking for whatever: " + whatever);
+}
