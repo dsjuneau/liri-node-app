619c704413ccf743705ddad2659e55010961c7c6
Scott Juneau <dsjuneau@gmail.com>
2019-05-10 20:09:16 -0500

    Done

--- liri.js
+++ liri.js
@@ -24,27 +24,32 @@ let inputValue =
     ? process.argv[3]
     : process.argv.splice(3).join(" ");
 
-//Take action based upon what is input.
-switch (inputSelector.indexOf(selector)) {
-  case 0:
-    concert(inputValue);
-    break;
-  case 1:
-    song(inputValue);
-    break;
-  case 2:
-    movie(inputValue);
-    break;
-  case 3:
-    whatever(inputValue);
-    break;
-  case -1:
-    console.log(
-      "Error.  PLease use one of the following selctors: concert-this, spotify-this-song, movie-this, or do-what-it-says."
-    );
-    break;
+function mainProgram(selector, inputValue) {
+  switch (inputSelector.indexOf(selector)) {
+    case 0:
+      createLog(selector + " " + inputValue + "\n");
+      concert(inputValue);
+      break;
+    case 1:
+      createLog(selector + " " + inputValue + "\n");
+      song(inputValue);
+      break;
+    case 2:
+      createLog(selector + " " + inputValue + "\n");
+      movie(inputValue);
+      break;
+    case 3:
+      createLog(selector + " " + inputValue + "\n");
+      whatever();
+      break;
+    case -1:
+      createLog(selector + " " + inputValue + "\n");
+      console.log(
+        "Error.  PLease use one of the following selctors: concert-this, spotify-this-song, movie-this, or do-what-it-says."
+      );
+      break;
+  }
 }
-
 function concert(concert) {
   const url =
     "https://rest.bandsintown.com/artists/" +
@@ -56,13 +61,23 @@ function concert(concert) {
     .then(function(response) {
       response.data.forEach(function(element) {
         console.log("Name of venue: " + element.venue.name);
+        createLog("Name of venue: " + element.venue.name + "\n");
         console.log(
           "Location: " + element.venue.city + ", " + element.venue.region
+        );
+        createLog(
+          "Location: " + element.venue.city + ", " + element.venue.region + "\n"
         );
         console.log(
           "Date of event: " + moment(element.datetime).format("MM/DD/YYYY")
         );
+        createLog(
+          "Date of event: " +
+            moment(element.datetime).format("MM/DD/YYYY") +
+            "\n"
+        );
         console.log("===========================================");
+        createLog("====================================================\n");
       });
     })
     .catch(function(error) {
@@ -93,13 +108,20 @@ function song(song) {
         "Preview Link: " + data.tracks.items[0].external_urls.spotify
       );
       console.log("Album Name: " + data.tracks.items[0].album.name);
+      createLog("Artist: " + data.tracks.items[0].artists[0].name + "\n");
+      createLog("Song Name: " + data.tracks.items[0].name + "\n");
+      createLog(
+        "Preview Link: " + data.tracks.items[0].external_urls.spotify + "\n"
+      );
+      createLog("Album Name: " + data.tracks.items[0].album.name + "\n");
+      createLog("====================================================\n");
     }
   );
 }
 
 function movie(movie) {
   const newMovie = movie === undefined ? "i=tt0485947" : "t=" + movie;
-  console.log(newMovie);
+
   axios
     .get(
       "http://www.omdbapi.com/?" + newMovie + "&y=&plot=short&apikey=trilogy"
@@ -109,7 +131,7 @@ function movie(movie) {
         response.data.Ratings[1] === undefined
           ? "no ratings"
           : response.data.Ratings[1].Value;
-      console.log(response.data);
+
       console.log("Title: " + response.data.Title);
       console.log("Year: " + response.data.Year);
       console.log("IMDB Rating: " + response.data.imdbRating);
@@ -118,6 +140,16 @@ function movie(movie) {
       console.log("Language: " + response.data.Language);
       console.log("Plot: " + response.data.Plot);
       console.log("Actors: " + response.data.Actors);
+
+      createLog("Title: " + response.data.Title + "\n");
+      createLog("Year: " + response.data.Year + "\n");
+      createLog("IMDB Rating: " + response.data.imdbRating + "\n");
+      createLog("Rotten Tomatoes: " + tomato + "\n");
+      createLog("Country: " + response.data.Country + "\n");
+      createLog("Language: " + response.data.Language + "\n");
+      createLog("Actors: " + response.data.Actors + "\n");
+      createLog("Plot: " + response.data.Plot + "\n");
+      createLog("====================================================\n");
     })
     .catch(function(error) {
       if (error.response) {
@@ -130,6 +162,22 @@ function movie(movie) {
     });
 }
 
-function whatever(whatever) {
-  console.log("looking for whatever: " + whatever);
+function createLog(info) {
+  fs.appendFile("log.txt", info, function(err) {
+    if (err) {
+      console.log(err);
+    }
+  });
 }
+
+function whatever() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    if (error) {
+      return console.log(error);
+    }
+    var dataArr = data.split(",");
+    mainProgram(dataArr[0], dataArr[1]);
+  });
+}
+
+mainProgram(selector, inputValue);
